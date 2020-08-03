f5c3f32f2c8a261a50a9e582aa9b6f1917c049d7
kavita00007 <kavitarpatil27@gmail.com>
2020-08-03 15:36:48 +0530

    This is my demo Java file

---
+++ src/practiceprograms/Practice.java
@@ -0,0 +1,11 @@
+package practiceprograms;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my demo java file");
+
+	}
+
+}
